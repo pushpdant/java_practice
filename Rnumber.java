9eb1404b4124baa96df6b7c89c6899950eea64a7
pushpdant <pushpdantjadhav14@gmail.com>
2022-03-07 16:39:58 +0530

    Advance for loop

---
+++ Rnumber.java
@@ -0,0 +1,26 @@
+import java.util.*;
+
+public class Rnumber{
+
+    public static void main(String args[])
+    {
+        int a[6],i,j;
+        Random r=new Random();
+        
+        
+        for(i=0;i<=6;i++)
+        {
+            a[i]=r.nextInt();
+        }
+
+        for(i=0;i<=6;i++)
+        {
+            system.out.println(a[i]);
+        }
+
+        
+    }
+
+
+
+}
